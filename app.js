b50798b16e5285853ba13507d42b27f72ac4e47c
GydoZajkowski <95379071+GydoZajkowski@users.noreply.github.com>
2022-03-24 17:19:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+
+const jsonData = JSON.stringify(data);
+
+const url = "https://us14.api.mailchimp.com/3.0/lists/3018d19737"
+
+const options = {
+  method: "POST",
+  auth: "Gydo:3b004cb7b782e5737eef6f0151568749-us14"
+}
+
+const request = https.request(url, options, function(response){
+
+  if (response.statusCode === 200) {
+    res.sendFile(__dirname + "/succes.html");
+  } else {
+    res.sendFile(__dirname + "/failure.html");
+  }
+
+
+  response.on("data", function(data){
+    console.log(JSON.parse(data));
+  })
+})
+
+request.write(jsonData);
+request.end();
+
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+})
+
+
+app.listen(3000, function(){
+  console.log("Server running on port 3000")
+});
